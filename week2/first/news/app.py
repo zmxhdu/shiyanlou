70f76b24b05bfe00dd2d07a730be5e4d850b390e
SKY-20150516HYE\Administrator <zmxhdu@126.com>
2017-09-24 01:06:35 +0800

    no message

--- week2/first/news/app.py
+++ week2/first/news/app.py
@@ -1,59 +1,58 @@
-from flask import Flask,render_template
-import json,os
+from flask import Flask, render_template
+import json, os
 from flask_sqlalchemy import SQLAlchemy
 
-app=Flask(__name__)
-app.config['TEMPLATE_AUTO_RELOAD']=True
-app.config['SQLALCHMNY_DATABASE_URI']='mysql://root@localhost/shiyanlou'
-db=SQLAlchemy(app)
+app = Flask(__name__)
+app.config['TEMPLATE_AUTO_RELOAD'] = True
+app.config['SQLALCHMNY_DATABASE_URI'] = 'mysql://root@localhost/shiyanlou'
+db = SQLAlchemy(app)
 
-jsonpath='/home/shiyanlou/files'
-jsonfilenames=os.listdir(jsonpath)
+jsonpath = '/home/shiyanlou/files'
+jsonfilenames = os.listdir(jsonpath)
+
 
 def jsonfile(jsonfilename):
-
-    with open(os.path.join(jsonpath,jsonfilename),'r') as jsonfile:
-        jsonfilecontent=json.loads(jsonfile.read())
+    with open(os.path.join(jsonpath, jsonfilename), 'r') as jsonfile:
+        jsonfilecontent = json.loads(jsonfile.read())
     return jsonfilecontent
 
 
 @app.route('/')
 def index():
-    titles=[]
+    titles = []
     for jsonfilename in jsonfilenames:
-        title=jsonfile(jsonfilename)['title']
+        title = jsonfile(jsonfilename)['title']
         titles.append(title)
     
-    files={
-        'titles':titles
+    files = {
+        'titles': titles
     }
 
-    return render_template('index.html',files=files)
+    return render_template('index.html', files=files)
 
 
 @app.route('/files/<filename>')
 def file(filename):
-    jsonfilename=filename+'.json'
+    jsonfilename = filename+'.json'
     if jsonfilename in jsonfilenames:
-        title=jsonfile(jsonfilename)['title']
-        created_time=jsonfile(jsonfilename)['created_time']
-        content=jsonfile(jsonfilename)['content']
+        title = jsonfile(jsonfilename)['title']
+        created_time = jsonfile(jsonfilename)['created_time']
+        content = jsonfile(jsonfilename)['content']
 
-        files={
-            'title':title,
-            'time':created_time,
-            'content':content
+        files = {
+            'title': title,
+            'time': created_time,
+            'content': content
         }
         return render_template('file.html', files=files)
     else:
         return not_found(404)
 
 
-
 @app.errorhandler(404)
 def not_found(error):
-    return render_template('404.html'),404
+    return render_template('404.html'), 404
 
 
-if __name__ =='__main__':
-    app.run(host='127.0.0.1',port=3000,debug=True)
+if __name__ == '__main__':
+    app.run(host = '127.0.0.1', port = 3000, debug = True)
